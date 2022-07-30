5ad10b0b437543b4df3de2f0ebb36d21c28ae00a
fanmiao11 <miao623@outlook.com>
2022-07-30 15:56:37 +0800

    element-demo

--- day02/element-demo/src/main.js
+++ day02/element-demo/src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 封装element插件
+import './plugins/element'
+
+
+// 全局引入element-ui
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI);
+
+// 按需引入
+// import ElementUI from 'element-ui';
+// Vue.use(ElementUI);
+
+// 引入部分组件
+// import { Button } from 'element-ui';
+// Vue.component(Button.name, Button);
+// Vue.use(Button)
+
 
 Vue.config.productionTip = false
 
